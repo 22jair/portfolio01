57fc50226133282f209a28a7d299e3103d41b1d5
22jair <jair_55_88@hotmail.com>
2022-02-23 01:17:03 -0500

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ import { IoLogoTwitter } from 'react-icons/io'
 export const Footer = () => {
   return (
     <footer>
-      <a href='#' className='footer__logo' >JAIR</a>
+      <a href='/#' className='footer__logo' >JAIR</a>
       <ul className='permalinks'>
         <li><a href='/#' >Home</a></li>
         <li><a href='/#about' >About</a></li>
